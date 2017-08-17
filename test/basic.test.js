2c9bf9299d4b90b0393387feaba8d3141c9176e4
holi <32414154@qq.com>
2017-08-17 22:04:13 +0800

    refactoring

--- test/basic.test.js
+++ test/basic.test.js
@@ -2,18 +2,23 @@ import ejs from "ejs"
 
 describe("ejs", ()=>{
 
-  test("plain text", ()=>{
+  it("plain text", ()=>{
     expect(ejs.render("text")).toBe("text")
   })
 
-  test("simple variable", ()=>{
-    expect(ejs.render("<%= foo %>", {foo:"bar"})).toBe("bar")
-  })
 
-  test("array", ()=>{
-    var data = {items: [1, 2, 3]}
+  describe("variables", ()=>{
 
-    expect(ejs.render("<%= items.join(', ') %>", data)).toBe("1, 2, 3")
+    it("basic", ()=>{
+      expect(ejs.render("<%= foo %>", {foo:"bar"})).toBe("bar")
+    })
+
+    it("array", ()=>{
+      var data = {items: [1, 2, 3]}
+
+      expect(ejs.render("<%= items.join(', ') %>", data)).toBe("1, 2, 3")
+    })
+
   })
 
 })
