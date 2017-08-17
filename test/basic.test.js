1a671d2e1c0eb543f1d64fc68922fa4ba9b435b9
holi <32414154@qq.com>
2017-08-17 22:28:59 +0800

    test ejs if statement

--- test/basic.test.js
+++ test/basic.test.js
@@ -10,13 +10,31 @@ describe("ejs", ()=>{
   describe("variables", ()=>{
 
     it("basic", ()=>{
-      expect(ejs.render("<%= foo %>", {foo:"bar"})).toBe("bar")
+      expect(ejs.render("<%= foo %>", {foo: "bar"})).toBe("bar")
     })
 
     it("array", ()=>{
       var data = {items: [1, 2, 3]}
 
       expect(ejs.render("<%= items.join(', ') %>", data)).toBe("1, 2, 3")
+    })
+
+  })
+
+
+  describe("flow control", ()=>{
+    
+    describe("if statement",()=>{
+      var tmpl = "<% if(typeof foo!=='undefined'){'bar'} %>"
+
+      it("absent", ()=>{
+        expect(ejs.render(tmpl)).toBe("")
+      })
+
+      it("present", ()=>{
+        expect(ejs.render(tmpl, {foo: true})).toBe("")
+      })
+
     })
 
   })
